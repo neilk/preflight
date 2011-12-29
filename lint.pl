1f6ede238ae76b59043bcb2368186d46ac1ed778
Neil Kandalgaonkar <neilk@brevity.org>
2011-12-29 12:34:46 -0800

    made conf file for linters

--- lint.pl
+++ lint.pl
@@ -2,18 +2,8 @@
 
 use strict;
 use File::Spec::Functions qw/curdir/;
+use File::Basename qw/dirname/;
 
-my %linter = (
-  'php' => sub { 
-    system( 'php -l ' . $_[0] );
-  },
-  'js' => sub { 
-    system( 'jslint ' . $_[0] );
-  },
-  'pl' => sub { 
-    system( 'perl -c ' . $_[0] );
-  },
-);
 
 sub getExtension {
   my ($file) = @_;
@@ -35,15 +25,31 @@ sub debug {
   }
 }
 
+sub getLinters {
+  my %linters;
+  my $progDir = dirname($0);
+  open my $cfgFh, '<', "$progDir/lint.conf" or die $!;
+  while (<$cfgFh>) {
+    chomp;
+    next if /^#/;
+    my ($ext, $cmd) = split ' ', $_, 2;
+    $linters{$ext} = sub {
+      system( $cmd . $_[0] );
+    };
+  }
+  close $cfgFh;
+  return \%linters;
+}
 
-
+my $linters = getLinters();
 while (<>) {
   chomp;
   my $file = $_;
   my $ext = getExtension($file);
-  if ( $ext and $linter{$ext} ) {
+  # might be useful to use magic on some files to determine what they are.
+  if ( $ext and $linters->{$ext} ) {
     warn "checking $file...\n";
-    $linter{$ext}->($file);
+    $linters->{$ext}->($file);
   }
 }
 
